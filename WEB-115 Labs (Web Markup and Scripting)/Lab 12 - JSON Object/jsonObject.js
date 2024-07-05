80e8ec1c1fcd9278b5c0a019f378678a6e0ac4b0
Alex Cesar <alex_cesar20@hotmail.com>
2024-07-05 16:30:22 -0400

    Lab 12 - JSON Object from WEB115 course

---
+++ WEB-115 Labs (Web Markup and Scripting)/Lab 12 - JSON Object/jsonObject.js
@@ -0,0 +1,23 @@
+document.getElementById("myButton").addEventListener("click", function() {
+    var firstName = document.getElementById("firstName").value;
+    var lastName = document.getElementById("lastName").value;
+    var course = document.getElementById("course").value;
+    var section = document.getElementById("section").value;
+    var role = document.getElementById("role").value.toLowerCase();
+
+    if (role !== "student" && role !== "instructor") {
+        window.alert("Only 'student' or 'instructor' is allowed for role.");
+
+        firstName, lastName, course, section, role = "";
+    } else {
+        var person = {
+            "firstName": `${firstName}`,
+            "lastName": `${lastName}`,
+            "course": `${course}`,
+            "section": `${section}`,
+            "role": `${role}`
+        };
+        
+        console.log(person);
+    };
+});
