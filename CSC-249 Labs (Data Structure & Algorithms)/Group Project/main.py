473ecfd7cb10f4088bce1fc63da10d8e5bca045a
Alex Cesar <alex_cesar20@hotmail.com>
2024-07-18 18:36:54 -0400

    Adding some of the structure of the project

---
+++ CSC-249 Labs (Data Structure & Algorithms)/Group Project/main.py
@@ -0,0 +1,102 @@
+#
+# Group: Smarty Pants
+# 7/23/2024
+#
+
+""" This program is an info game for helping the
+    user understand a few of data structures"""
+
+
+def main():
+    print("Welcome to the Data Structures info game!")
+
+    response = ""
+
+    while response != "0":
+        display_main_menu()
+        response = input('Enter your choice: ')
+
+        if response == "1":
+            from custom_array import Array, array_actions, display_info
+            action_response = ""
+
+            display_info()
+
+            # Ask user for an initial length of the array
+            capacity = int(input("How many initial elements do you want on the array? "))
+            initial_array = Array(capacity)
+
+            # Populate the array
+            for item in range(capacity - 1, -1, -1):
+                initial_array.insert(0, item)
+
+            # Decided to create the initial array for not confusing the user
+            print("An initial array was created.\n")
+            print("Go ahead and modify the elements for better understanding. Happy studies!")
+            print(f"Here is the initial array: {initial_array}")
+
+            while action_response != "0":
+                display_action_menu()
+                action_response = input('Enter your choice: ')
+                structure = array_actions(initial_array, action_response)
+
+        elif response == "2":
+            from listbag import ListBag, bag_actions
+            action_response = ""
+
+            initial_bag = ListBag()
+
+            while action_response != "0":
+                display_action_menu()
+                action_response = input('Enter your choice: ')
+                structure = bag_actions(initial_bag, action_response)
+
+        elif response == "3":
+            from liststack import ListStack, stack_actions
+            action_response = ""
+
+            initial_stack = ListStack()
+
+            while action_response != "0":
+                display_action_menu()
+                action_response = input('Enter your choice: ')
+                structure = stack_actions(initial_stack, action_response)
+
+        elif response == "4":
+            from listqueue import ListQueue, queue_actions
+            action_response = ""
+
+            initial_queue = ListQueue()
+
+            while action_response != "0":
+                display_action_menu()
+                action_response = input('Enter your choice: ')
+                structure = queue_actions(initial_queue, action_response)
+
+        elif response != "0":
+            print("Invalid choice. Try again.\n")
+
+    print("\nThank you for playing the Data Structures info game!")
+
+
+def display_main_menu():
+    print("\nWhat type of data structure do you want to learn?")
+    print("(1) Arrays\n"
+          "(2) Bags\n"
+          "(3) Stacks\n"
+          "(4) Queues\n"
+          "(0) Exit")
+
+
+def display_action_menu():
+    print("\nWhat action do you want to perform with the data structure you have chosen?")
+    print("(1) Add an element\n"
+          "(2) Remove an element\n"
+          "(3) Check the structure length\n"
+          "(4) Show elements of the structure\n"
+          "(5) Clear the structure\n"
+          "(0) Go back to the main menu")
+
+
+if __name__ == '__main__':
+    main()
